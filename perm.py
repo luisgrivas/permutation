ca7fac54f8bcebf9713177957930f7073bc8a87b
Luis Gonzalez <luisgzzrivas@gmail.com>
2018-04-18 01:11:02 -0500

    Update perm. Add demo.

--- perm.py
+++ perm.py
@@ -14,12 +14,13 @@ class Permutation:
     def __repr__(self):
         res = " "
         for ind in range(self.n_elements):
-              res = res + "X_" + str(ind) + " -> " + "X_" + str(self.mapping[ind]) + "\n "
+              res = res + str(ind) + " -> " + str(self.mapping[ind]) + "\n "
         return res
     
     def eval(self, element):
         return self.mapping[element]
  
+
 def lcm(*args):
     if len(args) == 1:
         return args[0] 
@@ -28,17 +29,25 @@ def lcm(*args):
     else:
         return lcm(args[0], lcm(args[1], args[2]))
 
+
+def equal(perm1, perm2):
+    if perm1.mapping == perm2.mapping:
+        return True
+    else:
+        return False
+
+
 def product(perm1, perm2):
     return Permutation([perm1.mapping[val] for val in perm2.mapping])
+    
 
-def iscycle(perm):
-    return False
-    
 def rand_permutation(n):
-    return Permutation(rand.sample([i for i in range(n)], n))
+    return Permutation(rand.sample(list(range(n)), n))
     
+
 def identity(n):
-    return Permutation([i for i in range(n)])
+    return Permutation(list(range(n)))
+
 
 def orbit(perm, element):
     ind = 0
@@ -47,6 +56,35 @@ def orbit(perm, element):
         res.append(perm.eval(res[ind]))
         ind +=1
     return set(res)
+
+
+def all_orbits(perm):
+    diff = set(range(perm.n_elements))
+    res = []
+    while bool(diff):
+        tmp = orbit(perm, list(diff)[0])
+        res.append(tmp)
+        diff = diff.difference(tmp)
+    return res
+
+
+def gen_cycle(perm, element):
+    tmp = orbit(perm, element)
+    return Permutation([index if index not in tmp else perm.eval(index)
+            for index in range(perm.n_elements)])
+
+
+def cycle_decom(perm):
+    tmp = all_orbits(perm)
+    return [gen_cycle(perm, list(index)[0]) for index in tmp if len(index) > 1]
+
+
+def iscycle(perm):
+    if len(cycle_decomposition(perm)) > 1:
+        return True
+    else:
+        return False
+
 
 def order(perm):
     tmp = [len(orbit(perm, val)) for val in range(perm.n_elements)]
@@ -62,3 +100,8 @@ def power(perm, n):
         return identity(perm.n_elements)
     else:
         return product(power(perm, n - 1), perm)
+
+
+def cyclic_group(perm):
+    n = order(perm)
+    return [power(perm, ind) for ind in range(n)]
